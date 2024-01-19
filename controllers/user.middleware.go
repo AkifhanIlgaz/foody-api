d2e4e411d70ff51275645fe31924cb8b96c5c5af
AkifhanIlgaz <akifhanilgaz@gmail.com>
2024-01-19 10:44:45 +0300

    implement getUserFromContext and RequireUser middleware

--- controllers/user.middleware.go
+++ controllers/user.middleware.go
@@ -1,9 +1,11 @@
 package controllers
 
 import (
+	"errors"
 	"log"
 	"net/http"
 
+	"github.com/AkifhanIlgaz/foody-api/models"
 	"github.com/AkifhanIlgaz/foody-api/services"
 	"github.com/gin-gonic/gin"
 )
@@ -39,9 +41,28 @@ func (middleware *UserMiddleware) SetUser() gin.HandlerFunc {
 	}
 }
 
-// TODO: Implement
 func (middleware *UserMiddleware) RequireUser() gin.HandlerFunc {
-	return func(ctx *gin.Context) {}
+	return func(ctx *gin.Context) {
+		_, err := getUserFromCtx(ctx)
+		if err != nil {
+			ctx.AbortWithError(http.StatusUnauthorized, err)
+			return
+		}
+
+		ctx.Next()
+	}
 }
 
-// TODO: Get user from ctx
+func getUserFromCtx(ctx *gin.Context) (*models.User, error) {
+	raw, exists := ctx.Get("currentUser")
+	if !exists {
+		return nil, errors.New("user not logged in")
+	}
+
+	user, ok := raw.(*models.User)
+	if !ok {
+		return nil, errors.New("wrong type for user")
+	}
+
+	return user, nil
+}
